48c0389bf963293496635be8e216168fae7385c9
ark0f <arsenylyashenko.3@gmail.com>
2019-02-27 17:04:44 +0300

    Specify crate version in README.md for docs.rs badge

---
+++ tests/version_sync.rs
@@ -0,0 +1,7 @@
+use version_sync::assert_contains_regex;
+
+#[test]
+fn readme() {
+    assert_contains_regex!("README.md", r#"https://docs\.rs/{name}/badge\.svg\?version={version}"#);
+    assert_contains_regex!("README.md", r#"https://docs\.rs/{name}/{version}"#);
+}
