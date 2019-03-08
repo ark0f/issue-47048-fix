4de9024a3405492f9966845a0301842d3f5f0e35
ark0f <arsenylyashenko.3@gmail.com>
2019-03-08 18:25:40 +0300

    Cancel Github Pages deployment

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 /// Compile C file with fix and add to your executable
+/// with targets like "pc-windows-gnu"
 ///
 /// `build.rs`:
 /// ```no_run
@@ -11,22 +12,21 @@
 /// }
 /// ```
 ///
-#[cfg(all(target_os = "windows", target_env = "gnu"))]
 pub fn issue_47048_fix() {
-    use std::{env, fs::File, io::Write, path::Path};
+    #[cfg(all(target_os = "windows", target_env = "gnu"))]
+    {
+        use std::{env, fs::File, io::Write, path::Path};
 
-    let path = env::var("OUT_DIR").expect("Failed to get 'OUT_DIR' environment variable");
-    let path = Path::new(&path).join("issue_47048_fix.c");
+        let path = env::var("OUT_DIR").expect("Failed to get 'OUT_DIR' environment variable");
+        let path = Path::new(&path).join("issue_47048_fix.c");
 
-    if !path.exists() {
-        let mut out_file =
-            File::create(path.clone()).expect("Failed to create file 'issue_47048_fix.c'");
-        write!(&mut out_file, "{}", include_str!("fix.c"))
-            .expect("Failed to write content into file 'issue_47048_fix.c'");
+        if !path.exists() {
+            let mut out_file =
+                File::create(path.clone()).expect("Failed to create file 'issue_47048_fix.c'");
+            write!(&mut out_file, "{}", include_str!("fix.c"))
+                .expect("Failed to write content into file 'issue_47048_fix.c'");
+        }
+
+        cc::Build::new().file(path).compile("issue_47048_fix")
     }
-
-    cc::Build::new().file(path).compile("issue_47048_fix")
 }
-
-#[cfg(not(all(target_os = "windows", target_env = "gnu")))]
-pub fn issue_47048_fix() {}
